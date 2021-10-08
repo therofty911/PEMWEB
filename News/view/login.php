fa547ca4fc5565a0f41026350d3c401cec53dd16
Rofty <57068821+therofty911@users.noreply.github.com>
2021-10-08 20:46:35 +0700

    login page change

--- News/view/login.php
+++ News/view/login.php
@@ -1,10 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Mazer Admin Dashboard</title>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="../assets/css/app.css">
+    <link rel="stylesheet" href="../assets/css/pages/auth.css">
+</head>
+<style>
+  #auth #auth-right{
+    background:linear-gradient(90deg,#0D1A44,#0D1A44);
+  }
+  .btn-check:focus + .btn-primary, .btn-primary:focus, .btn-primary:hover{
+    background-color: #3950a2;
+    border-color: #364b98;
+    color: #fff;
+  }
+</style>
+<body style="overflow-x: hidden;">
+    <div id="auth">
+        
+<div class="row h-100">
+    <div class="col-lg-5 col-12 justify-content-center text-center">
+        <div id="auth-left">
+            <div class="auth-logo">
+                <a href="index.html"><img src="../assets/images/logo/logo.png" alt="Logo" style="width: 250px;height: auto;"></a>
+            </div>
+            <h1 class="auth-title" style="color: #0D1A44;">Log in.</h1>
+            <p class="auth-subtitle mb-2">Silahkan Login ke News Speedy UMN</p>
+
+            <form action="index.html">
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="text" class="form-control form-control-xl" placeholder="Username">
+                    <div class="form-control-icon">
+                        <i class="bi bi-person"></i>
+                    </div>
+                </div>
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="password" class="form-control form-control-xl" placeholder="Password">
+                    <div class="form-control-icon">
+                        <i class="bi bi-shield-lock"></i>
+                    </div>
+                </div>
+                <div class="g-recaptcha" data-sitekey="6LdFmmQcAAAAAGuC6N1MNLbDMeSLwB8n1PR512k8" style="margin-bottom: 10px;"></div>
+
+                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" style="background-color: #0D1A44;">Log in</button>
+            </form>
+            <div class="text-center mt-5 text-lg fs-4">
+                <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Signup</a>.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right" >
+        </div>
+    </div>
+</div>
+
+    </div>
+</body>
+
+</html>
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="../assets/css/login.css" /> -->
+    <link rel="stylesheet" href="../assets/css/login.css" />
     <title>Sign in & Sign up Form</title>
   </head>
 <style>
@@ -474,7 +549,7 @@
       </div>
     </div>
 
-    <!-- <script src="../assets/js/login.css"></script> -->
+    <script src="../assets/js/login.css"></script>
     <script>
         const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
@@ -492,8 +567,8 @@ sign_in_btn.addEventListener("click", () => {
 </html>
 
 <?php
-  include '..\..\News\controller\login.php';
-  if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signin'])){
-    verifiedlogin();
-  }
-?>
+  // include '..\..\News\controller\login.php';
+  // if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signin'])){
+  //   verifiedlogin();
+  // }
+?> -->
