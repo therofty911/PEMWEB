8805a57dea016a58707886e049e44e6a1ca5e620
Rofty <57068821+therofty911@users.noreply.github.com>
2021-12-03 23:56:30 +0700

    1

--- fasilitas/application/views/pages/home_book.php
+++ fasilitas/application/views/pages/home_book.php
@@ -4,7 +4,7 @@
             <h2 class="mt-4 mb-4" style="color: #BAA360;">Booking Form</h2>
         </div>
         <div class="col-md-6 col-lg-6  position-absolute top-50 start-50 translate-middle card-box">
-            <form class="row g-3" method="post" action="<?= base_url('auth/book'); ?>">
+            <form class="row g-3" method="post" action="<?= base_url('home/book'); ?>">
                 <div class="col-md-12">
                     <label for="id" class="form-label text-light">Facility ID</label>
                     <input type="text" class="form-control" id="id" name="id" disabled>
